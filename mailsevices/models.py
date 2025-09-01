73d4965b5547d74d0aaa055f85f5c8d77664691b
grigorii goncharov <grigoriy85@gmail.com>
2025-09-01 19:10:31 +0300

    создана модель Recipient в прилодении mailservices

--- mailsevices/models.py
+++ mailsevices/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Recipient(models.Model):
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email",
+        help_text="Уникальный адрес электронной почты получателя"
+    )
+    full_name = models.CharField(
+        max_length=255,
+        verbose_name="Ф. И. О.",
+        help_text="Полное имя получателя"
+    )
+    comment = models.TextField(
+        blank=True, null=True,
+        verbose_name="Комментарий",
+        help_text="Дополнительная информация о получателе"
+    )
+
+    class Meta:
+        verbose_name = "Получатель рассылки"
+        verbose_name_plural = "Получатели рассылки"
+
+    def __str__(self):
+        return f"{self.full_name} ({self.email})"
+
